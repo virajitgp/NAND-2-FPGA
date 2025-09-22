dee7d955a65ff84878551ae81f0bc6835c2036f8
ryu <virajitgp@gmail.com>
2025-09-22 10:32:41 +0530

    Update ROM32K.v
    no init

--- project_5/ROM32K.v
+++ project_5/ROM32K.v
@@ -6,14 +6,6 @@ module ROM32K(
 
     reg [15:0] rom [0:32767];
 
-    initial begin
-        // Pre-load the ROM with a simple program
-        // @100
-        rom[0] = 16'b0000000001100100;
-        // M=1
-        rom[1] = 16'b1110111111001000;
-    end
-
     always @* begin
         out = rom[address];
     end
